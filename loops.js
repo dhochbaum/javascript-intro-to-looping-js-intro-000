46fc30e11a15ab3620f6de19c962ac370a8c95cf
David Hochbaum <dhochbaum+flatiron@gmail.com>
2019-05-31 20:08:12 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,10 +1,14 @@
 function forLoop(a) {
   for (let i = 0; i < 25; i++) {
+    /*
     if (i===1) {
       a[i] = "I am 1 strange loop."
     } else {
       a[i] = `I am ${i} strange loops.`
     }
+    */
+    a[i] = `I am ${i} strange loops.`
   }
+  a['1'] = "I am 1 strange loop."
   return a
 }
